cdb102e37710a9c6a007e0d64ee1204ee17264c0
raula09 <davitoniani0602@gmail.com>
2025-12-25 00:18:08 +0400

    init

---
+++ src/AndroidMicSystem.Shared.UI/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AndroidMicSystem.Shared.UI.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
